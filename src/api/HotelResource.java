8da59ce785e988ee6dd40c3fe5df622b9c895581
thomas <historian.thomas@gmail.com>
2021-10-10 17:19:36 -0500

    Created the HotelResource class that interacts with both CustomerService class and ReservationService

---
+++ src/api/HotelResource.java
@@ -0,0 +1,56 @@
+package api;
+
+import model.Customer;
+import model.Reservation;
+import model.Room;
+import service.CustomerService;
+import service.ReservationService;
+
+import java.time.LocalDate;
+
+/**
+ * This API class will interact with the service class
+ */
+public class HotelResource {
+
+    static CustomerService customerServiceSession;
+    static ReservationService reservationServiceSession;
+
+    public HotelResource(){
+        super();
+        customerServiceSession = new CustomerService();
+        reservationServiceSession = new ReservationService();
+    }
+
+    public Customer getCustomer(String email){
+
+        return customerServiceSession.getCustomer(email);
+
+    }
+
+
+    public void createACustomer(String email, String firstName, String lastName){
+
+        customerServiceSession.addCustomer(email,firstName,lastName);
+
+    }
+
+    public Room getRoom(String roomNumber){
+
+       return reservationServiceSession.getARoom(roomNumber);
+
+    }
+
+    public void bookARoom(Customer customer, Room room, LocalDate checkInDate, LocalDate checkOutDate){
+
+        reservationServiceSession.reserveARoom(customer, room, checkInDate, checkOutDate);
+
+    }
+
+    public Reservation findARoom(LocalDate checkIn, LocalDate checkOut){
+
+        return reservationServiceSession.findRooms( checkIn, checkOut);
+
+    }
+
+}
